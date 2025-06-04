d28baafd5785a4e2ad1e533c0a86e5357a5e64e3
eunji <moeunji12@gmail.com>
2025-06-04 11:33:26 +0900

    day15

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// 헤더 페이드인 인터랙션
 const header = document.querySelector("header");
 let posY = 0;
 // 스크롤의 위치값을 담는 변수가 될거임
@@ -18,3 +19,18 @@ function headerHide() {
 function headershow() {
     header.classList.remove("hide");
 }
+
+
+// 스크롤페이드인 인터랙션
+const content = document.getElementsByClassName("content");
+window.addEventListener("scroll",()=>{
+    const winH = window.innerHeight;
+    for(let i = 0; i < content.length; i++){
+        const contentTop = content[i].getBoundingClientRect().top;
+        if(contentTop - winH < 0){
+            content[i].classList.add("in");
+        }else {
+            content[i].classList.remove("in");
+        }
+    }
+});
